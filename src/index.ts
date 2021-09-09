86d1fc3d69214e3d17ed7463d395a65561c8cdb6
menttofly <1028365614@qq.com>
2021-09-09 14:39:53 +0800

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import { Probot } from "probot";
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Thanks for opening this issue! I will check it out later.",
     });
+    context.log.info("Receive issues opened event")
     await context.octokit.issues.createComment(issueComment);
   });
   // For more information on building apps:
